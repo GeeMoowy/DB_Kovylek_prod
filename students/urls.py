1fb4578cda4df7214ea6a224d29007493da0b42a
geemoowy <kovylek.ul@gmail.com>
2025-08-14 09:39:54 +0300

    added students_list.html

--- students/urls.py
+++ students/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from students.apps import StudentsConfig
-from students.views import MainView
+from students.views import MainView, StudentsListView
 
 
 app_name = StudentsConfig.name
 
 urlpatterns=[
     path('', MainView.as_view(), name='main'),
+    path('students_list/', StudentsListView.as_view(), name='students_list')
 ]
